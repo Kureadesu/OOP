46ed904d97330be5b2e6158cc3c4b79323955330
Kureadesu <118508974+Kureadesu@users.noreply.github.com>
2023-10-02 17:36:28 +0800

    Add files via upload

---
+++ OOP-main/TestApp2/src/testapp2/Transaction.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testapp2;
+
+/**
+ *
+ * @author Kurea
+ */
+
+public class Transaction {
+    public static void main(String[] args) {
+        
+    }
+}
